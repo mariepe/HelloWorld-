2ca35c3d4506b453dd12762222089c7bc1f8304a
Pelz.Marie <Pelz.Marie@SK-448A5B8FB91E.2631.skool.net>
2016-09-22 08:18:47 +0200

    ;-)

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,9 +1,14 @@
 
 public class HelloWorld {
 
+	static int zahl1 = 45;
+	static int zahl2 = 12;
+	
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-
+		System.out.println(zahl1/zahl2);
+		System.out.println(zahl1%zahl2);
+		
 	}
 
 }
