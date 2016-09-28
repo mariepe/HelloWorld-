e60310bb0dc7d11f373398954746a0f2a35b0066
mariepelz <mariepelz@maries-mbp>
2016-09-28 17:51:26 +0200

    ;-)

---
+++ HelloWorld/src/Primzahlen.java
@@ -0,0 +1,36 @@
+
+public class Primzahlen {
+	
+	static int number = 0;
+			
+
+	public static void main(String[] args) {
+		int ausgabe=0;
+	System.out.println ("Primzahlen");
+	
+	for(int i=number; i<= 10000; i++);{
+
+
+	if(isPrime(number)==true){
+	System.out.print(number + ", ");
+	ausgabe++;
+	
+	if(ausgabe%30==0){
+		System.out.println("");
+		ausgabe=0;
+	}
+	}
+	}
+	}
+ 
+	public static boolean isPrime(int number ) {
+		
+		if( number <2){return false;}
+		
+		for(int i=2; i<=number/2; i++){
+				if(number%i==0){
+						return false;}
+				}
+		return true;
+		}
+	}
