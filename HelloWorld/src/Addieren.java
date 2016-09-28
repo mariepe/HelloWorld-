25159ac12e8986f04a743429f9787b683c8dfa86
mariepelz <mariepelz@maries-mbp>
2016-09-28 18:20:34 +0200

    :)

---
+++ HelloWorld/src/Addieren.java
@@ -0,0 +1,11 @@
+
+public class Addieren {
+
+	public static void main(String[] args) {
+		
+		System.out.println(addiere(1,2));
+	}
+	public static int addiere(int zahl1, int zahl2){
+		return zahl1 + zahl2;
+}
+}
