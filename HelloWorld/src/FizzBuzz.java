3159fceed8c13e0ca6499af0506e117120429c07
mariepelz <mariepelz@maries-mbp>
2016-10-24 20:53:34 +0200

    hello

---
+++ HelloWorld/src/FizzBuzz.java
@@ -0,0 +1,25 @@
+
+public class FizzBuzz {
+
+	public static void main(String[] args) {
+		
+		int n=5;
+		
+		
+		
+		if (n%3==0 && n%5==0){
+			System.out.println("FizzBuzz");
+		}
+		
+			else if (n%3==0){
+			System.out.println("Fizz");
+		}
+		
+			else if (n%5==0){
+				System.out.println("Buzz");
+			}
+		
+		}
+	
+}
+
